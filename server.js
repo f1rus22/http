c73dda3e2089e25cfcdf1cd33eb75bf061cb1fd3
Alexander_Botya <f1rus22@gmail.com>
2022-08-08 17:01:53 +0300

    изм.3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 
 
-filenames = fs.readdirSync('./files');
+
 
 
 const host = 'localhost';
@@ -11,27 +11,36 @@ const port = 8000;
 
 
 const requestListener = (req, res) =>{
-    if(req.method === 'POST' && req.url ==='/post'){
-        res.writeHead(200);
-        res.end('sucess');
-    }else if ( req.method != 'POST' && req.url === '/post'){
-        res.writeHead(405);
-        res.end('HTTP method not allowed');
-    } else if(req.method === 'DELETE' && req.url ==='/delete'){
-        res.writeHead(200);
-        res.end('sucess');
-    }else if ( req.method != 'DELETE' && req.url === '/delete'){
-        res.writeHead(405);
-        res.end('HTTP method not allowed');
+    if(req.url ==='/post'){
+        if (req.method === 'POST'){
+            res.writeHead(200);
+            res.end('sucess');
+        } else {
+            res.writeHead(405);
+            res.end('HTTP method not allowed');
+        }
+    } else if(req.url ==='/delete'){
+        if (req.method === 'DELETE'){
+            res.writeHead(200);
+            res.end('sucess');
+        } else {
+            res.writeHead(405);
+            res.end('HTTP method not allowed');  
+        }
     } else if (req.method != 'GET'){
         res.writeHead(405);
         res.end('HTTP method not allowed');
-    } else if(filenames.toString() === ''){
-        res.writeHead(500);
-        res.end('Internal server error');
     } else if(req.url === '/get'){
-        res.writeHead(200);
-        res.end('Filenames in directory: ' + filenames.toString());
+        if (req.method === 'GET'){
+            try {
+                const filenames = fs.readdirSync('./files');
+                res.writeHead(200);
+                res.end('Filenames in directory: ' + filenames.toString());
+            } catch (error){
+                res.writeHead(500);
+                res.end('Internal server error');
+            }
+        } 
     }else if (req.url === '/redirect'){
         res.writeHead(302, {
             Location: "/redirected"
