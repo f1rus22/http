7b2462e6e0a5043c26dc4eb1e539c81549ca065f
Alexander_Botya <f1rus22@gmail.com>
2022-08-08 14:38:23 +0300

    исправление ошибок 1

--- server.js
+++ server.js
@@ -2,31 +2,45 @@ const http = require('http');
 const fs = require('fs');
 
 
+
 filenames = fs.readdirSync('./files');
+
 
 const host = 'localhost';
 const port = 8000;
+
 
 const requestListener = (req, res) =>{
     if(req.method === 'POST' && req.url ==='/post'){
         res.writeHead(200);
         res.end('sucess');
+    }else if ( req.method != 'POST' && req.url === '/post'){
+        res.writeHead(405);
+        res.end('HTTP method not allowed');
     } else if(req.method === 'DELETE' && req.url ==='/delete'){
         res.writeHead(200);
         res.end('sucess');
+    }else if ( req.method != 'DELETE' && req.url === '/delete'){
+        res.writeHead(405);
+        res.end('HTTP method not allowed');
     } else if (req.method != 'GET'){
         res.writeHead(405);
         res.end('HTTP method not allowed');
-    } else if(filenames.toString() === ''){
+    } else if(filenames === false){
         res.writeHead(500);
         res.end('Internal server error');
     } else if(req.url === '/get'){
         res.writeHead(200);
         res.end('Filenames in directory: ' + filenames.toString());
     }else if (req.url === '/redirect'){
-        res.writeHead(301);
-        res.end('new url: /redirect');
-    }else {
+        res.writeHead(302, {
+            Location: "/redirected"
+        });
+        res.end();
+    } else if (req.url === '/redirected'){
+            res.writeHead(200);
+            res.end("the new url: /redirected");
+        }else { 
     res.writeHead(404);
     res.end('Not found')
     }
@@ -38,4 +52,3 @@ const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Сервер запущен и доступен по адресу http://${host}:${port}`);
 });
-
